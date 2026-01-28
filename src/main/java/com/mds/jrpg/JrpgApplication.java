c319fc6e99ac947cda5169905e6ba4ac26db3cce
user <user@MacBook-Air-de-user.local>
2026-01-28 10:46:54 +0100

    innit

---
+++ src/main/java/com/mds/jrpg/JrpgApplication.java
@@ -0,0 +1,13 @@
+package com.mds.jrpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrpgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mds/jrpg/JrpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mds.jrpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
