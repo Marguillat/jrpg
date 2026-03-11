918c171dfd35f6b59a73b5c22b288a1879096a9b
user <user@MacBook-Air-de-user.local>
2026-03-11 15:48:04 +0100

    connexion mongodb

--- src/main/java/com/mds/jrpg/greeting/GreetingController.java
+++ src/main/java/com/mds/jrpg/greeting/GreetingController.java
@@ -1,7 +1,6 @@
 package com.mds.jrpg.greeting;
 
 import java.util.concurrent.atomic.AtomicLong;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-  private static final String TEMPLATE = "Hello, %s!";
-  private final AtomicLong counter = new AtomicLong();
+    private static final String TEMPLATE = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    private final GreetingRepository greetingRepository;
 
-  @GetMapping("/")
-  public String home() {
-    return "Welcome to JRPG API";
-  }
+    public GreetingController(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
 
-  @GetMapping("/greeting")
-  public GreetingRecord greeting(@RequestParam(defaultValue = "World") String name) {
-    return new GreetingRecord(counter.incrementAndGet(), TEMPLATE.formatted(name));
-  }
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to JRPG API";
+    }
+
+    @GetMapping("/greeting")
+    public GreetingRecord greeting(
+        @RequestParam(defaultValue = "World") String name
+    ) {
+        GreetingRecord record = new GreetingRecord(
+            null,
+            counter.incrementAndGet(),
+            TEMPLATE.formatted(name)
+        );
+        return greetingRepository.save(record);
+    }
 }
