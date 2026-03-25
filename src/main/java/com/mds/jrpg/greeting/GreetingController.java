c8f44e89786bca06ccd93ad3f605cff66fd7ca28
user <user@MacBook-Air-de-user.local>
2026-03-25 11:08:26 +0100

    refactoring et tabspaces

--- src/main/java/com/mds/jrpg/greeting/GreetingController.java
+++ src/main/java/com/mds/jrpg/greeting/GreetingController.java
@@ -8,28 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String TEMPLATE = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-    private final GreetingRepository greetingRepository;
+  private static final String TEMPLATE = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+  private final GreetingRepository greetingRepository;
 
-    public GreetingController(GreetingRepository greetingRepository) {
-        this.greetingRepository = greetingRepository;
-    }
+  public GreetingController(GreetingRepository greetingRepository) {
+    this.greetingRepository = greetingRepository;
+  }
 
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to JRPG API";
-    }
+  @GetMapping("/")
+  public String home() {
+    return "Welcome to JRPG API";
+  }
 
-    @GetMapping("/greeting")
-    public GreetingRecord greeting(
-        @RequestParam(defaultValue = "World") String name
-    ) {
-        GreetingRecord record = new GreetingRecord(
-            null,
-            counter.incrementAndGet(),
-            TEMPLATE.formatted(name)
-        );
-        return greetingRepository.save(record);
-    }
+  @GetMapping("/greeting")
+  public GreetingRecord greeting(
+    @RequestParam(defaultValue = "World") String name
+  ) {
+    GreetingRecord record = new GreetingRecord(
+      null,
+      counter.incrementAndGet(),
+      TEMPLATE.formatted(name)
+    );
+    return greetingRepository.save(record);
+  }
 }
--- src/main/java/com/mds/jrpg/greeting/GreetingRepository.java
+++ src/main/java/com/mds/jrpg/greeting/GreetingRepository.java
@@ -5,4 +5,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GreetingRepository
-    extends MongoRepository<GreetingRecord, String> {}
+  extends MongoRepository<GreetingRecord, String> {}
